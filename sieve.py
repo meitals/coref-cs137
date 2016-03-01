482369655a06bbfd4adc802823d19bc24fed660e
Meital Singer <tali324@comcast.net>
2016-02-29 21:07:23 -0500

    minor changes

--- sieve.py
+++ sieve.py
@@ -36,6 +36,7 @@ class Sieve(object):
 			doc.chains = exact_match(doc.entities, doc.chains)
 			#doc.chains = precise_constructs(doc.entities, doc.chains, doc)
 			#doc.chains = cluster_head_match(doc.entities, doc.chains)
+			#doc.chains = word_inclusion(doc.entities, doc.chains)
 			
 			########END SIEVES###########
 
